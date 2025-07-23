61bf6a95c6b097bc6a1fa3605c26ab2f6e3f0b47
Aishani <aishaniv30@gmail.com>
2025-07-23 15:41:08 -0500

    Initial Commit

---
+++ Interactive/InteractiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveApp.swift
+//  Interactive
+//
+//  Created by Ranjan Verma on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
